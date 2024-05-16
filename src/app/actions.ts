0faf929a8595bb0bbde5a9666d6a3fd199f99703
insd47 <me@insd.dev>
2024-05-16 17:13:04 +0900

    fix: missing api key

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -3,7 +3,11 @@ import { Profile } from "../lib/types";
 
 export const useProfiles = () => {
   const fetchProfiles = async () =>
-    await fetch(import.meta.env.VITE_API_URL + "/profiles").then((res) => {
+    await fetch(import.meta.env.VITE_API_URL + "/profiles", {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
+      },
+    }).then((res) => {
       if (res.ok) return res.json() as unknown as Profile[];
       else
         throw new Error(
